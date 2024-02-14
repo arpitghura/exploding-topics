6a8a82934bba39ff24227fdd5547c9c71a78931c
arpitghura <ghurarpit110@gmail.com>
2024-02-14 21:36:29 +0530

    added Header with Navigation Menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
-import Image from "next/image";
+import Navbar from "@/comps/Navbar";
+import { Fragment } from "react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hello</h1>
-    </main>
+    <Fragment>
+      <Navbar />
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>Hello</h1>
+      </main>
+    </Fragment>
   );
 }
