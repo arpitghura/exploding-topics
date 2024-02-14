dfdb6280c2213bd02a791c801331779033cb546b
arpitghura <ghurarpit110@gmail.com>
2024-02-14 22:53:15 +0530

    added footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/comps/Footer";
 import Navbar from "@/comps/Navbar";
 import { Fragment } from "react";
 
@@ -5,9 +6,10 @@ export default function Home() {
   return (
     <Fragment>
       <Navbar />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {/* <main className="flex min-h-screen flex-col items-center justify-between p-24">
         <h1>Hello</h1>
-      </main>
+      </main> */}
+      <Footer />
     </Fragment>
   );
 }
