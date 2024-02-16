1d5cf94634a219ac9706fd18e4c3892b71e64404
arpitghura <ghurarpit110@gmail.com>
2024-02-16 14:27:28 +0530

    adds pagination and footer seperation with background effects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,14 @@ import DataCard from "@/comps/DataCard";
 import Footer from "@/comps/Footer";
 import HeroSec from "@/comps/Hero";
 import Navbar from "@/comps/Navbar";
+import PaginationSec from "@/comps/PaginationSec";
 import { Fragment } from "react";
 
 export default function Home() {
   return (
     <Fragment>
       <Navbar />
-      <main className="container p-8 flex min-h-screen flex-col items-center">
+      <main className="container p-8 pb-0 flex min-h-screen flex-col items-center">
         <HeroSec />
         <div className="container p-8 flex flex-row items-center justify-between gap-8 flex-wrap">
           <DataCard isProOnly={false} />
@@ -20,6 +21,7 @@ export default function Home() {
           <DataCard isProOnly={false} />
           <DataCard isProOnly={false} />
           <DataCard isProOnly={true} />
+          <PaginationSec />
         </div>
       </main>
       <Footer />
---
+++ src/comps/PaginationSec.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import {
+  Pagination,
+  PaginationContent,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination";
+import { useState } from "react";
+
+const PaginationSec = () => {
+  const [currPage, setCurrentPage] = useState(1);
+  return (
+    <div className="pagination flex flex-start">
+      <Pagination>
+        <PaginationContent>
+          <PaginationItem>
+            {currPage !== 1 && (
+              <PaginationPrevious
+                href="#"
+                className="p-1 border border-whitesmoke py-0 h-7 w-7 hover:border-black"
+              />
+            )}
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink
+              href="#"
+              isActive={true}
+              className="border border-whitesmoke opacity-100 border-black  mx-1 p-1 h-7 w-7"
+            >
+              1
+            </PaginationLink>
+            <PaginationLink
+              href="#"
+              className="border border-whitesmoke opacity-60 hover:opacity-100 hover:border-black mx-1 p-1 h-7 w-7"
+            >
+              2
+            </PaginationLink>
+            <PaginationLink
+              href="#"
+              className="border border-whitesmoke opacity-60 hover:opacity-100 hover:border-black mx-1 p-1 h-7 w-7"
+            >
+              3
+            </PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationNext
+              href="#"
+              className="p-1 border border-whitesmoke py-0 h-7 w-7 hover:border-black"
+            />
+          </PaginationItem>
+        </PaginationContent>
+      </Pagination>
+    </div>
+  );
+};
+
+export default PaginationSec;
