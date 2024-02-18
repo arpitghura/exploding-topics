b2088246a5f9a19a43f15e0835564b159337884b
arpitghura <ghurarpit110@gmail.com>
2024-02-18 15:02:21 +0530

    added search by time frame and category

---
+++ src/comps/CategorySelection.tsx
@@ -0,0 +1,157 @@
+"use client";
+import * as React from "react";
+import Link from "next/link";
+
+const components: { title: string; href: string }[] = [
+  {
+    title: "Featured",
+    href: "#",
+  },
+  {
+    title: "AI",
+    href: "#",
+  },
+  {
+    title: "Beauty",
+    href: "#",
+  },
+  {
+    title: "Company",
+    href: "#",
+  },
+  {
+    title: "Concept",
+    href: "#",
+  },
+  {
+    title: "Design",
+    href: "#",
+  },
+  {
+    title: "Eco",
+    href: "#",
+  },
+  {
+    title: "Ecommerce",
+    href: "#",
+  },
+  {
+    title: "Education",
+    href: "#",
+  },
+  {
+    title: "Fashion",
+    href: "#",
+  },
+  {
+    title: "Finance",
+    href: "#",
+  },
+  {
+    title: "Fitness",
+    href: "#",
+  },
+  {
+    title: "Food",
+    href: "#",
+  },
+  {
+    title: "Gaming",
+    href: "#",
+  },
+  {
+    title: "Health",
+    href: "#",
+  },
+  {
+    title: "Home",
+    href: "#",
+  },
+  {
+    title: "Lifestyle",
+    href: "#",
+  },
+  {
+    title: "Luxury",
+    href: "#",
+  },
+  {
+    title: "Marketing",
+    href: "#",
+  },
+  {
+    title: "Media",
+    href: "#",
+  },
+  {
+    title: "Pets",
+    href: "#",
+  },
+  {
+    title: "Product",
+    href: "#",
+  },
+  {
+    title: "Sales",
+    href: "#",
+  },
+  {
+    title: "Science",
+    href: "#",
+  },
+  {
+    title: "Social",
+    href: "#",
+  },
+  {
+    title: "Software",
+    href: "#",
+  },
+  {
+    title: "Sports",
+    href: "#",
+  },
+  {
+    title: "Startup",
+    href: "#",
+  },
+  {
+    title: "Technology",
+    href: "#",
+  },
+  {
+    title: "Travel",
+    href: "#",
+  },
+];
+
+export function CategorySelection() {
+  return (
+    <div className="border hover:border-black">
+      <button
+        className="flex py-1 px-3 text-base"
+        onClick={() => {
+          document.querySelector("#allCategories")?.classList.toggle("hidden");
+        }}
+      >
+        All Categories
+      </button>
+      <div
+        className="w-[calc(100vw_/_2)] absolute gap-3 p-4 -left-full top-9 flex flex-wrap justify-center items-center bg-white rounded z-20 hidden"
+        id="allCategories"
+      >
+        {components.map((component) => (
+          <Link
+            key={component.title}
+            href={component.href}
+            className={`w-max border border-blue-100 bg-blue-100 hover:border-blue-600 hover:bg-blue-100 hover:text-primary p-2 text-primary rounded font-semibold ${
+              component.title === "Featured" && "bg-blue-500 text-white"
+            }`}
+          >
+            {component.title}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+}
--- src/comps/Hero.tsx
+++ src/comps/Hero.tsx
@@ -1,4 +1,5 @@
-import Image from "next/image";
+import { CategorySelection } from "./CategorySelection";
+import SelectCard from "./SelectCard";
 
 const HeroSec = () => {
   return (
@@ -9,81 +10,14 @@ const HeroSec = () => {
       <p className="">
         We surface rapidly growing topics before they take off.
       </p>
-      <div className="sortBar hidden">
-        <div className="mobileSelectorDarkBg"></div>
-        <div className="dropdownOnlySelectorsPlusProToggleContainer topicsPagedropdownOnlySelectorsPlusProToggleContainer">
-          <div className="dropdownOnlySelectors dropdownOnlySelectorsFilters flex">
-            <div className="sortBarPretext">Filter By:</div>
-            <div className="filterDropdownsAddRow freeFilters">
-              <div className="filterDropdownsAddInnerRow ">
-                <div className="filterContainer periodFilterContainer freeToProPeriodSelector">
-                  <div className="trendDbPageButtonDropdown btn-group">
-                    <div className="dropdownFilterButton noselect">
-                      <span className="filterButtonText dropdownFilterButtonText no_space_wrap">
-                        5 years
-                      </span>
-                      <svg
-                        style={{ marginLeft: "14px" }}
-                        width="14"
-                        height="8"
-                        viewBox="0 0 14 8"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M1 1L7 7L13 1"
-                          stroke="#A0A2AF"
-                          strokeLinecap="round"
-                        ></path>
-                      </svg>
-                    </div>
-                  </div>
-                </div>
-                <div className="filterContainer periodFilterContainer">
-                  <div className="trendDbPageButtonDropdown btn-group">
-                    <div className="dropdownFilterButton noselect">
-                      <span className="filterButtonText dropdownFilterButtonText">
-                        All Categories
-                      </span>
-                      <svg
-                        style={{ marginLeft: "14px" }}
-                        width="14"
-                        height="8"
-                        viewBox="0 0 14 8"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M1 1L7 7L13 1"
-                          stroke="#A0A2AF"
-                          strokeLinecap="round"
-                        ></path>
-                      </svg>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="filterDropdownsAddInnerRow"></div>
-            </div>
-            <div className="freeToProInput flex items-center gap-2 border border-[#121737] bg-white p-2 rounded-sm">
-              <svg width="14" height="14" viewBox="0 0 14 14" fill="none">
-                <path
-                  d="M9.86401 9.86401C11.864 7.864 11.864 4.62135 9.86401 2.62134C7.864 0.621327 4.62135 0.621327 2.62134 2.62134C0.621328 4.62135 0.621328 7.864 2.62134 9.86401C4.62135 11.864 7.864 11.864 9.86401 9.86401ZM9.86401 9.86401L12.58 12.58"
-                  stroke="#A0A2AF"
-                  strokeWidth="1.5"
-                  strokeLinecap="round"
-                ></path>
-              </svg>
-              <input placeholder="Search Trends" />
-              <div className="freeToProSVG">
-                <Image
-                  src={"/freeToPro.svg"}
-                  width="42"
-                  height="25"
-                  alt="pro"
-                />
-              </div>
-            </div>
+      <div>
+        <div className="search-options flex items-center justify-center gap-3 w-full">
+          <span className=" text-sm uppercase font-bold text-nowrap">
+            Filter By:
+          </span>
+          <SelectCard />
+          <div className="w-full relative">
+            <CategorySelection />
           </div>
         </div>
       </div>
